b941168267f9e72499116fd619d08b1bf161a712
TanishaRathore5334 <tanisharathore011@gmail.com>
2025-06-02 18:18:00 +0530

    Update Bubble.java

--- sorting/Bubble.java
+++ sorting/Bubble.java
@@ -14,18 +14,18 @@ import java.util.*;
 //                 }
 //         }
 //     }
-//     public static void printArr(int arr[]) {
-//         for(int i=0; i<arr.length; i++){
-//             System.out.println(arr[i]+" ");
-//         }
-//         System.out.println();
-//     }
-//     public static void main(String[] args) {
-//         int arr[] = {5,4,1,3,2};
-//         bubbleSort(arr);
-//         printArr(arr);
-//         }
-//     }
+    public static void printArr(int arr[]) {
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,2,8,9,78};
+        bubbleSort(arr);
+        printArr(arr);
+        }
+    }
 
 
 
