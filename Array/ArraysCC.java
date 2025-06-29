4504be4a6e9cedc1612fa5494c50f5b5df28c7ac
TanishaRathore5334 <tanisharathore011@gmail.com>
2025-06-29 15:29:11 +0530

    Update ArraysCC.java

--- Array/ArraysCC.java
+++ Array/ArraysCC.java
@@ -8,16 +8,13 @@ public class ArraysCC{
         marks[1]=sc.nextInt();
         marks[2]=sc.nextInt();
         marks[3]=sc.nextInt();
-        marks[4]=sc.nextInt();
-
-    
+        
         System.out.println("phy:" + marks[0]);
         System.out.println("chem:" + marks[1]);
         System.out.println("math:" + marks[2]);
         System.out.println("hin:" + marks[3]);
-        System.out.println("eng:" + marks[4]);
-
-        int percentage = (marks[0]+marks[1]+marks[2]+marks[3]+marks[4]) / 5;
+    
+        int percentage = (marks[0]+marks[1]+marks[2]+marks[3]) / 4;
         System.out.println("percentage: = " + percentage + "%");
     }
 }
