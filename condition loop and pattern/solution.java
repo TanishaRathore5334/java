c17e7d052b60718fe15a4ea120ebfd1ace50733c
TanishaRathore5334 <tanisharathore011@gmail.com>
2025-07-02 21:22:26 +0530

    Update solution.java

--- condition loop and pattern/solution.java
+++ condition loop and pattern/solution.java
@@ -4,9 +4,9 @@ public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 int x = sc.nextInt();
 if (x > 0) {
-System.out.println("x is greater than 0");
+System.out.println("x is a greater than 0");
 } else {
-System.out.println("x is less than or equal 0");
+System.out.println("x is a less than or equal 0");
 }
 }
 }
