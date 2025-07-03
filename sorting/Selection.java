6de4a797b185d908667a43e5b6a17c6a14beb488
TanishaRathore5334 <tanisharathore011@gmail.com>
2025-07-03 23:00:46 +0530

    Update Selection.java

--- sorting/Selection.java
+++ sorting/Selection.java
@@ -21,23 +21,23 @@ public static void printArr(int arr[]) {
         System.out.println();
         }
 
-    public static void selection(int arr[]) {
-        for(int i=0; i<arr.length; i++){
-            int minPos = i;
-            for(int j=i+1; j<arr.length; j++){
-                if(arr[j]<arr[minPos]){
-                    minPos = j;
-                }
-        }
-        //swap
-        int temp = arr[i];  
-        arr[i] = arr[minPos];
-            arr[i] = temp;
-    }
-}
-    public static void main(String[] args) {
-        int arr[] = {5,4,1,3,2};
-        selectionSort(arr);
-        printArr(arr);
-        }
-    }
+//     public static void selection(int arr[]) {
+//         for(int i=0; i<arr.length; i++){
+//             int minPos = i;
+//             for(int j=i+1; j<arr.length; j++){
+//                 if(arr[j]<arr[minPos]){
+//                     minPos = j;
+//                 }
+//         }
+//         //swap
+//         int temp = arr[i];  
+//         arr[i] = arr[minPos];
+//             arr[i] = temp;
+//     }
+// }
+    // public static void main(String[] args) {
+    //     int arr[] = {5,4,1,3,2};
+    //     selectionSort(arr);
+    //     printArr(arr);
+    //     }
+    // }
