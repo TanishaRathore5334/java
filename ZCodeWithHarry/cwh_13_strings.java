d5d158b6551471429599f2fc0c071f2630737f4e
TanishaRathore5334 <tanisharathore011@gmail.com>
2025-07-07 23:43:52 +0530

    Update cwh_13_strings.java

--- ZCodeWithHarry/cwh_13_strings.java
+++ ZCodeWithHarry/cwh_13_strings.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class cwh_13_strings {
     public static void main(String[] args) {
         // String name = new String("harry");
-        // String name = "harry";
+        // String name = "Tanisha";
         // System.out.println("This name is:");
         // System.out.println(name);
 
